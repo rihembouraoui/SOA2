43a2211e4aa9706b9697a1d65b05ab1bf44a91f2
rihembouraoui <147322202+rihembouraoui@users.noreply.github.com>
2023-10-19 12:34:23 +0100

    COMMIT

---
+++ TP2/src/Service/BanqueService.java
@@ -0,0 +1,8 @@
+package Service;
+
+public class BanqueService {
+	double convertion (double mantant ) {
+		return mantant *3.6;
+	}
+
+}
---
+++ TP2/src/Service/Main.java
@@ -0,0 +1,11 @@
+package Service;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		BanqueService p = new BanqueService();
+		System.out.print(p.convertion(50));
+	}
+
+}
